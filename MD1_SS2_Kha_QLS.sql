ef4471505d34de5d7880c1a06fc8e99444949119
AnhQuocHuynh <146312927+AnhQuocHuynh@users.noreply.github.com>
2025-11-03 03:11:45 +0700

    Add files via upload

---
+++ MD1_SS2_Kha_QLS.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE LibraryDB;
+CREATE SCHEMA library;
+
+CREATE TABLE library.Books(
+	book_id SERIAL PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+	author VARCHAR(50) NOT NULL,
+	publish_year INT,
+	price FLOAT
+)
